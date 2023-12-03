92589370517040ad24aae84c0fac049c94942f9a
MoldyLunchBox <duu116@gmail.com>
2023-12-03 15:37:42 +0100

    slider needs smooth effect

--- store/src/component/Slider.jsx
+++ store/src/component/Slider.jsx
@@ -10,28 +10,34 @@ export const Slider = () => {
         "https://images.pexels.com/photos/1138560/pexels-photo-1138560.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         "https://images.pexels.com/photos/4210866/pexels-photo-4210866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     ]
-        const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-        const nextSlide = () => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }
 
-        const prevSlide = () => {
-            setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-        }
+    const prevSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    }
 
-        return (
-            <div className="relative">
-      <div className="flex justify-between absolute w-full top-1/2 transform -translate-y-1/2">
-        <button onClick={prevSlide} className="text-white bg-gray-800 p-2 rounded-full focus:outline-none">
-          Prev
-        </button>
-        <button onClick={nextSlide} className="text-white bg-gray-800 p-2 rounded-full focus:outline-none">
-          Next
-        </button>
-      </div>
-      <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} className="w-full max-w-lg h-auto" />
-    </div>
-        )
-    
+    return (
+        <div className='max-w-[500px] h-[500px]'>
+            <div className="relative flex justify-center items-center  w-full h-full bg-red-200">
+                <div className="flex justify-between absolute w-full top-1/2 transform -translate-y-1/2">
+                    <button onClick={prevSlide} className="text-white bg-gray-800 p-2 rounded-full focus:outline-none">
+                        Prev
+                    </button>
+                    <button onClick={nextSlide} className="text-white bg-gray-800 p-2 rounded-full focus:outline-none">
+                        Next
+                    </button>
+                </div>
+                <img
+                    src={images[currentIndex]}
+                    alt={`Slide ${currentIndex}`}
+                    className="object-cover w-auto h-[500px] transition-opacity duration-500 ease-in-out"
+                />
+            </div>
+        </div>
+    )
+
 }
